f29e89a17d10ac7f938d74af0b87839c973761c5
nguyenthiminhanh2 <nstella080@gmail.com>
2025-03-29 22:51:26 +0700

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,36 @@
+fetch("./js/data.json")
+    .then(resp => resp.json())
+    .then(coffee => {
+        console.log(coffee)
+        // Lấy ra thẻ có id là "row_api"
+        let container = document.querySelector('#row_api')
+        
+        // Dùng vòng lặp for từ i = 0 tới độ dài của mảng coffee
+        for (let i = 0; i < coffee.length; i++) {
+            console.log(coffee[i].image)
+            let item = `
+                <div class="col-md-4">
+                    <div class="cream_box" data-id="${coffee[i].id}">
+                        <div class="cream_img">
+                            <img src="${coffee[i].image}" alt="${coffee[i].title}">
+                        </div>
+                        <div class="price_text">$${coffee[i].price.toFixed(2)}</div>
+                        <h6 class="strawberry_text">${coffee[i].title}</h6>
+                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
+                    </div>
+                </div>
+            `
+            console.log(item)
+            container.innerHTML += item
+        }
+        // Thêm sự kiện click cho từng sản phẩm
+        document.querySelectorAll('.cream_box').forEach(item => {
+            item.addEventListener('click', function () {
+                let id = this.getAttribute('data-id');
+                // Chuyển hướng sang trang chi tiết sản phẩm
+                window.location.href = `product-detail.html?id=${id}`;
+            })
+        })
+    })
+
+    .catch(error => console.error(error));
---
+++ product-detail.js
@@ -0,0 +1,26 @@
+// Lấy id từ URL
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+
+// Kiểm tra nếu có id thì mới thực hiện fetch
+if (productId) {
+    fetch('./js/data.json')
+        .then(resp => resp.json())
+        .then(coffee => {
+            // Tìm sản phẩm có id khớp với productId
+            const product = coffee.find(item => item.id == productId);
+
+            if (product) {
+                // Đổ dữ liệu sản phẩm vào trang chi tiết
+                document.querySelector('.product-title').innerText = product.title;
+                document.querySelector('.product-image').src = product.image;
+                document.querySelector('.product-price').innerText = `$${product.price.toFixed(2)}`;
+                document.querySelector('.product-description').innerText = product.description;
+            } else {
+                console.error('Không tìm thấy sản phẩm');
+            }
+        })
+        .catch(error => console.error('Lỗi khi lấy dữ liệu:', error));
+} else {
+    console.error('Không tìm thấy id sản phẩm trong URL');
+}
